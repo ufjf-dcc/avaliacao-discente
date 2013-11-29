6c76381de0379c7c19af73e3458ecd15be0edb89
Pedro Bittencourt <pedroantonio.bittencourt@gmail.com>
2013-11-29 02:24:38 -0200

    Implementada lógica para recuperar as respostas
    Foi implementada a lógica para pegar as respostas que os alunos davam às perguntas, ainda não está salvando nada no banco de dados.

--- src/br/ufjf/avaliacao/controller/AvaliacoesDisponiveisController.java
+++ src/br/ufjf/avaliacao/controller/AvaliacoesDisponiveisController.java
@@ -1,5 +1,6 @@
 package br.ufjf.avaliacao.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.HibernateException;
@@ -8,25 +9,33 @@ import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.Init;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zul.Messagebox;
 import org.zkoss.zul.Row;
 import org.zkoss.zul.Window;
 
 import br.ufjf.avaliacao.model.Avaliacao;
+import br.ufjf.avaliacao.model.Pergunta;
 import br.ufjf.avaliacao.model.Questionario;
+import br.ufjf.avaliacao.model.Resposta;
 import br.ufjf.avaliacao.persistent.impl.QuestionarioDAO;
 
 public class AvaliacoesDisponiveisController extends GenericController {
 
-	private List<Avaliacao> avaliacaoProfs;
-	private static Avaliacao avaliacaoAtual;
 	private QuestionarioDAO questionarioDAO = new QuestionarioDAO();
-	private List<Questionario> questionariosCoord = questionarioDAO.retornaQuestinariosParaUsuarioCoord(usuario);
-	private List<Questionario> questionariosProfs = questionarioDAO.retornaQuestinariosParaUsuario(usuario);
+	private List<Questionario> questionariosCoord = questionarioDAO
+			.retornaQuestinariosParaUsuarioCoord(usuario);
+	private List<Questionario> questionariosProfs = questionarioDAO
+			.retornaQuestinariosParaUsuario(usuario);
 	private List<Questionario> questionariosAuto = questionarioDAO
 			.retornaQuestinariosCursoTipo(usuario.getCurso(), 2);
 	private List<Questionario> questionariosInfra = questionarioDAO
 			.retornaQuestinariosCursoTipo(usuario.getCurso(), 3);
-	private static Questionario questionarioAtual;
+	private static Questionario questionarioAtual = new Questionario();
+	private Avaliacao avaliacao = new Avaliacao();
+	private Resposta resposta = new Resposta();
+	private List<Resposta> respostas = new ArrayList<Resposta>();
 
 	@Init
 	public void init() throws HibernateException, Exception {
@@ -34,10 +43,8 @@ public class AvaliacoesDisponiveisController extends GenericController {
 	}
 
 	@Command
-	@SuppressWarnings("static-access")
-	public void avaliar(@BindingParam("avaliacao") Avaliacao avaliacao) {
-		this.avaliacaoAtual = avaliacao;
-		this.questionarioAtual = avaliacaoAtual.getQuestionario();
+	public void avaliar(@BindingParam("questionario") Questionario questionario) {
+		AvaliacoesDisponiveisController.questionarioAtual = questionario;
 		Window window = (Window) Executions.createComponents("/avaliar.zul",
 				null, null);
 		window.doModal();
@@ -80,6 +87,45 @@ public class AvaliacoesDisponiveisController extends GenericController {
 		}
 	}
 
+	@Command
+	public void terminarAvaliacao() {
+		
+		if(respostas.size()!=questionarioAtual.getPerguntas().size()) {
+			Messagebox.show("Responda todas as perguntas antes de finalizar!");
+		} else {
+			for (Resposta r : respostas) {
+				System.out.println(r.getResposta());
+			}
+			Messagebox.show("Avaliação Salva com Sucesso", "Concluído",
+					Messagebox.OK, Messagebox.INFORMATION,
+					new EventListener<Event>() {
+						@Override
+						public void onEvent(Event event)
+								throws Exception {
+							Executions.sendRedirect(null);
+						}
+					});
+		}
+		
+	}
+
+	@Command
+	public void escolha(@BindingParam("string") String escolha,
+			@BindingParam("pergunta") Pergunta perg) {
+
+		resposta.setResposta(escolha);
+		resposta.setPergunta(perg);
+
+		for (int i = 0; i < respostas.size() ; i++)
+			if (respostas.get(i).getPergunta() == resposta.getPergunta()) {
+				respostas.remove(respostas.get(i));
+				break;
+			}
+
+		respostas.add(resposta);
+		resposta = new Resposta();
+	}
+
 	public List<Questionario> getQuestionariosCoord() {
 		return questionariosCoord;
 	}
@@ -120,20 +166,28 @@ public class AvaliacoesDisponiveisController extends GenericController {
 		AvaliacoesDisponiveisController.questionarioAtual = questionarioAtual;
 	}
 
-	public List<Avaliacao> getAvaliacaoProfs() {
-		return avaliacaoProfs;
+	public Avaliacao getAvaliacao() {
+		return avaliacao;
 	}
 
-	public void setAvaliacaoProfs(List<Avaliacao> avaliacaoProfs) {
-		this.avaliacaoProfs = avaliacaoProfs;
+	public void setAvaliacao(Avaliacao avaliacao) {
+		this.avaliacao = avaliacao;
 	}
 
-	public Avaliacao getAvaliacaoAtual() {
-		return avaliacaoAtual;
+	public Resposta getResposta() {
+		return resposta;
 	}
 
-	public void setAvaliacaoAtual(Avaliacao avaliacaoAtual) {
-		AvaliacoesDisponiveisController.avaliacaoAtual = avaliacaoAtual;
+	public void setResposta(Resposta resposta) {
+		this.resposta = resposta;
+	}
+
+	public List<Resposta> getRespostas() {
+		return respostas;
+	}
+
+	public void setRespostas(List<Resposta> respostas) {
+		this.respostas = respostas;
 	}
 
 }
